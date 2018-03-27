9a888dc2f8754d3eefbd83a4a533a1b29f19fd4c
Bozsum/Bozsumwv <bozsumwv@gmail.com>
2018-03-27 13:21:14 +0700

    edit release 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("line 1 feature a");
+        System.out.println("edit release 1");
     }
 }
